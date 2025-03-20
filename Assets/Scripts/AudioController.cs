a183a10a9974c5a6cd0855ffce283c01c25bd9aa
Willian Chiquinato <willianchiquinato@hotmail.com>
2025-03-19 21:20:19 -0300

    Demo com Audio

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    private static AudioController instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
